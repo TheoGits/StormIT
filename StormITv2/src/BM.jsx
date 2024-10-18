07076eb15828f94e30d18b095a32ec668492978d
TheoGits <theokrist07@gmail.com>
2024-10-18 15:56:31 +0200

    forsøk 14

--- StormITv2/src/BM.jsx
+++ StormITv2/src/BM.jsx
@@ -7,11 +7,8 @@ const BackgroundMusic = () => {
     const playAudio = () => {
       if (audioRef.current) {
         audioRef.current.volume = 1; // Sett volumet til 1 (fullt volum)
-        console.log('Prøver å spille av lyd...');
-        audioRef.current.play().then(() => {
-          console.log('Lydavspilling startet!');
-        }).catch((error) => {
-          console.error('Autoplay ble blokkert. Brukerinteraksjon kreves for å starte lyd: ', error);
+        audioRef.current.play().catch((error) => {
+          console.log('Autoplay ble blokkert. Brukerinteraksjon kreves for å starte lyd: ', error);
         });
       }
     };
@@ -26,10 +23,9 @@ const BackgroundMusic = () => {
 
   return (
     <div>
-      <audio ref={audioRef} loop onCanPlay={() => audioRef.current.play()}>
-       <source src="/music.mp3" type="audio/mpeg" />
+      <audio ref={audioRef} loop>
+        <source src="/music.mp3" type="audio/mpeg" />
       </audio>
-
     </div>
   );
 };
--- StormITv2/vite.config.js
+++ StormITv2/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'StormIT', // Bytt ut 'repository-name' med navnet på GitHub-repositoriet ditt
+  base: '/', // Bytt ut 'repository-name' med navnet på GitHub-repositoriet ditt
 });
